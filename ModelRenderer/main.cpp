b4a0aaf1e6294c6cc51ec315963b31b2c1a28ec7
kabapoo <dr.kabapoo@gmail.com>
2021-09-08 19:13:46 +0900

    first commit

---
+++ ModelRenderer/main.cpp
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int main()
+{
+	glewExperimental = true;
+
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Model Renderer", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to open GLFW window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+	return 0;
+}
---
+++ ModelRenderer/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <assimp/Importer.hpp>
+
+#define SCR_WIDTH 1024
+#define SCR_HEIGHT 1024
+
+#endif
